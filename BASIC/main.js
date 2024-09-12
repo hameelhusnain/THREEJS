b9970723bcb5117c79f707c8a6f019fb8a747a3e
hameelhusnain <husnainhameel3@gmail.com>
2024-09-12 18:05:04 +0500

    create renderer

--- BASIC/main.js
+++ BASIC/main.js
@@ -10,10 +10,18 @@ camera.position.z = 5;
 
 
 // 3. Create & add a cude object
+const geometry = new THREE.BoxGeometry();
+const material  = new THREE.MeshBasicMaterial({ color: '#468585', emissive: '#468585' });
 
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 // 4. Add lighting 
-
+const light = new THREE.DirectionalLight(0x9CDBA6, 10);
+light.position.set(1, 1, 1);
+scene.add(light);
 // 5. Setup the renderer
-
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 // 6. Animate the scene
