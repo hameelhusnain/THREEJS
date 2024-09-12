3d694eccf18bb3a71ab2aac2da2363675d23f0e5
hameelhusnain <husnainhameel3@gmail.com>
2024-09-12 18:09:38 +0500

    edit js file

--- BASIC/main.js
+++ BASIC/main.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 
 //1. Create the scene 
-const scene = new THREE.scene();
-scene.background = new THREE.color('#F0F0F0')
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0')
 
 // 2. Add the camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
