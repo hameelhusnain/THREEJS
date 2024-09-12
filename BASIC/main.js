b6d14e0f16b320f41aec5f3b5a59391aa353531d
hameelhusnain <husnainhameel3@gmail.com>
2024-09-12 16:38:46 +0500

    ADD CAMERA SETTING

--- BASIC/main.js
+++ BASIC/main.js
@@ -5,8 +5,12 @@ const scene = new THREE.scene();
 scene.background = new THREE.color('#F0F0F0')
 
 // 2. Add the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 5;
+
 
 // 3. Create & add a cude object
+
 
 // 4. Add lighting 
 
