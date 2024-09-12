0c50c2f5653837539f06c3b57d21f2e88f160d56
hameelhusnain <husnainhameel3@gmail.com>
2024-09-12 16:31:25 +0500

    Created new js file and start editing it

---
+++ BASIC/main.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+
+//1. Create the scene 
+const scene = new THREE.scene();
+scene.background = new THREE.color('#F0F0F0')
+
+// 2. Add the camera
+
+// 3. Create & add a cude object
+
+// 4. Add lighting 
+
+// 5. Setup the renderer
+
+// 6. Animate the scene
