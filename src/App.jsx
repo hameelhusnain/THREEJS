fe4c7f84fe7dcb2fe623fcc85de1627203cd7800
hameelhusnain <husnainhameel3@gmail.com>
2024-09-15 22:25:47 +0500

    2 commits

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
-      <h1 className="text-2xl text-white underline">Hello Three JS</h1>
+      <Navbar/>
     </main>
   )
 }
